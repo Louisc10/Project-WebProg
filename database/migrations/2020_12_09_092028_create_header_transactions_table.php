5f39a6cf7ca3ed76fbb61fbb6336df108ad12c81
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-12-09 16:23:07 +0700

    create mcr header transaction

---
+++ database/migrations/2020_12_09_092028_create_header_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHeaderTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('header_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('transaction_date', 0);
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('header_transactions');
+    }
+}
