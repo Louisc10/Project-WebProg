2c548393f01a42d4c81a0c2819d7587fe25a030b
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-12-09 16:31:26 +0700

    create mcr detail transaction

---
+++ database/migrations/2020_12_09_092417_create_detail_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transactions', function (Blueprint $table) {
+            $table->unsignedBigInteger('transaction_id');
+            $table->unsignedBigInteger('shoes_id');
+            $table->int('quantity');
+            $table->foreign('transaction_id')->reference('id')->on('header_transactions');
+            $table->foreign('shoes_id')->reference('id')->on('shoes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transactions');
+    }
+}
