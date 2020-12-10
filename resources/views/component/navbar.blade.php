5bb621210cffadcc69b81f55ac8e027738cf81be
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-12-10 16:20:06 +0700

    install bootstrap, create nav and login view

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,18 @@
+<div class="navbar navbar-expand sticky-top navbar-light" style="background-color: #e3f2fd;">
+    <a class="navbar-brand " href="#">Just Du It</a>
+    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item m-1">
+                <a href="/login">
+                    <button type="button" class="btn btn-primary">Login</button>
+                </a>
+            </li>
+            <li class="nav-item m-1">
+                <a href="/login">
+                    <button type="button" class="btn btn-primary">Register</button>
+                </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</div>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('title')
+    Login Page
+@endsection
+
+@section('content')
+    <div class="containter m-4">
+        <div class="row justify-content-md-center">
+            <div class="col-6">
+                <form>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                    </div>
+                    <div class="form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="checkRememberMe">Remember Me</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+            </div>
+        </div>
+    </div>
+@endsection
